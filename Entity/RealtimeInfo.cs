b3eb0c8573e3d74d3db1ebe37e58a359df332104
Sacrify <peng.wade@gmail.com>
2015-02-07 15:38:51 +0800

    Add logic for realtime
    Add logic for realtime

---
+++ Entity/RealtimeInfo.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CJHSJ_OilWebView.Entity
+{
+    [DataContract]
+    public class RealtimeInfo
+    {
+        public RealtimeInfo()
+        { }
+        private string _mmsi; //唯一标识
+        private string _name; // 
+        private string _stime;	//
+        private string _shorttime;
+        private string _pos_x;	//
+        private string _pos_y;		//
+        private string _speed; //
+        private string _llun_rps;		//
+        private string _rlun_rps;	//
+        private string _lmain_oil_gps;	//
+        private string _lmain_oil_accgps;	//
+        private string _rmain_oil_gps;	//
+        private string _rmain_oil_accgps;	//
+        private string _lasist_oil_gps;	//
+        private string _lasist_oil_accgps;	//
+        private string _rasist_oil_gps;	//
+        private string _rasist_oil_accgps;	//
+        private string _interval_minute; // Offline check
+        private string _drift_interval;	//
+        private string _drift_accinterval;	//
+        private string _lmpg_status;
+        private string _rmpg_status;
+        private string _lrun_time;
+        private string _rrun_time;
+        private string _sail_time;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string mmsi
+        {
+            set { _mmsi = value; }
+            get { return _mmsi; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string name
+        {
+            set { _name = value; }
+            get { return _name; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string stime
+        {
+            set { _stime = value; }
+            get { return _stime; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string shorttime
+        {
+            set { _shorttime = value; }
+            get { return _shorttime; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string pos_x
+        {
+            set { _pos_x = value; }
+            get { return _pos_x; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string pos_y
+        {
+            set { _pos_y = value; }
+            get { return _pos_y; }
+        }
+        /// <summary>
+        /// 个性签名
+        /// </summary>
+        [DataMember]
+        public string speed
+        {
+            set { _speed = value; }
+            get { return _speed; }
+        }
+        /// <summary>
+        /// 真实姓名
+        /// </summary>
+        [DataMember]
+        public string llunrps
+        {
+            set { _llun_rps = value; }
+            get { return _llun_rps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string rlunrps
+        {
+            set { _rlun_rps = value; }
+            get { return _rlun_rps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string lmain_gps
+        {
+            set { _lmain_oil_gps = value; }
+            get { return _lmain_oil_gps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string lmain_accgps
+        {
+            set { _lmain_oil_accgps = value; }
+            get { return _lmain_oil_accgps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string rmain_gps
+        {
+            set { _rmain_oil_gps = value; }
+            get { return _rmain_oil_gps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string rmain_accgps
+        {
+            set { _rmain_oil_accgps = value; }
+            get { return _rmain_oil_accgps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string lasist_gps
+        {
+            set { _lasist_oil_gps = value; }
+            get { return _lasist_oil_gps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string lasist_accgps
+        {
+            set { _lasist_oil_accgps = value; }
+            get { return _lasist_oil_accgps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string rasist_gps
+        {
+            set { _rasist_oil_gps = value; }
+            get { return _rasist_oil_gps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string rasist_accgps
+        {
+            set { _rasist_oil_accgps = value; }
+            get { return _rasist_oil_accgps; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string interval_minute
+        {
+            get { return _interval_minute; }
+            set { _interval_minute = value; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string drift_interval
+        {
+            set { _drift_interval = value; }
+            get { return _drift_interval; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [DataMember]
+        public string drift_accval
+        {
+            set { _drift_accinterval = value; }
+            get { return _drift_accinterval; }
+        }
+
+        [DataMember]
+        public string lmpg_status
+        {
+            set { _lmpg_status = value; }
+            get { return _lmpg_status; }
+        }
+
+        [DataMember]
+        public string rmpg_status
+        {
+            set { _rmpg_status = value; }
+            get { return _rmpg_status; }
+        }
+
+        [DataMember]
+        public string lrun_time
+        {
+            set { _lrun_time = value; }
+            get { return _lrun_time; }
+        }
+
+        [DataMember]
+        public string rrun_time
+        {
+            set { _rrun_time = value; }
+            get { return _rrun_time; }
+        }
+
+        [DataMember]
+        public string sail_time
+        {
+            set { _sail_time = value; }
+            get { return _sail_time; }
+        }
+    }
+}
--- shipoil_ajax.aspx.cs
+++ shipoil_ajax.aspx.cs
@@ -27,12 +27,49 @@ namespace CJHSJ_OilWebView
         private string etime;
         private int pageSize = 0;
         private int oilType = 0;
+        private string companyID = "0";
 
         private const int SEASON_SUMMER = 5;
         private const int SEASON_WINTER = 11;
-        private double oilDensitySummer = 0.84;
-        private double oilDensityWinter = 0.86;
+        private const double OIL_DENSITY_SUMMER = 0.84;
+        private const double OIL_DENSITY_WINTER = 0.86;
+        private double oilDensitySummer = OIL_DENSITY_SUMMER;
+        private double oilDensityWinter = OIL_DENSITY_WINTER;
         private double oilExKgPerHour = 3.0;
+
+        private enum DURATION_TYPE
+        {
+            NONE = 0,
+            SAIL_TIME,
+            RUNNING_TIME_LEFT,
+            RUNNING_TIME_RIGHT,
+            RUNNING_TIME
+        }
+
+        public DbOperHandler dohOil;
+
+        override protected void OnInit(EventArgs e)
+        {
+            Server.ScriptTimeout = 90;//默认脚本过期时间
+            base.OnInit(e);
+
+            //油耗数据操作类
+            if (dohOil == null)
+            {
+                System.Data.SqlClient.SqlConnection conn = new System.Data.SqlClient.SqlConnection(System.Configuration.ConfigurationManager.AppSettings["ConnectionString"].ToString());
+                dohOil = new SqlDbOperHandler(conn);
+            }
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            base.OnUnload(e);
+            if (dohOil != null) 
+            {
+                dohOil.Dispose();
+                dohOil = null;
+            }
+        }
 
         public bool CheckFormUrl()
         {
@@ -89,9 +126,57 @@ namespace CJHSJ_OilWebView
 
             switch (this._operType) 
             {
+                case "getShipInfo":
+                    getShipInfo();
+                    break;
+
+                case "getRealtimeStat":
+                    getShipOilInfo();
+                    getRealtimeStat();
+                    break;
+
                 default:
                     break;
             }
+        }
+
+        private ShipInfo getShipInfoByMMSI()
+        {
+            dohOil.Reset();
+            dohOil.SqlCmd = "SELECT TOP 1 * " +
+                "FROM " + "ship_reginfo " +
+                "WHERE " + "mmsi=" + "'" + mmsi + "'";
+
+            DataTable dt = dohOil.GetDataTable();
+            if (dt.Rows.Count > 0)
+            {
+                ShipInfo si = new ShipInfo();
+                si.mmsi = dt.Rows[0]["mmsi"].ToString();
+                si.shipname = dt.Rows[0]["ship_name"].ToString();
+                si.speed = dt.Rows[0]["speed"].ToString();
+                si.load_weight = dt.Rows[0]["load_weight"].ToString();
+                si.draft = dt.Rows[0]["draft"].ToString();
+                si.llunsp = dt.Rows[0]["llun_speed"].ToString();
+                si.rlunsp = dt.Rows[0]["rlun_speed"].ToString();
+                return si;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private void getShipInfo()
+        {
+            List<ShipInfo> items = new List<ShipInfo>();
+            ShipInfo si = getShipInfoByMMSI();
+
+            if (si != null)
+            {
+                items.Add(si);
+            }
+
+            //输出返回
+            dtHelp.ResutJsonStr((Object)items);
         }
 
         private void getShipOilInfo()
@@ -101,43 +186,214 @@ namespace CJHSJ_OilWebView
                 return;
             }
 
-            //dohOil.Reset();
-            //string sqlCmd =
-            //    "SELECT TOP 1 * " +
-            //    "FROM ship_reginfo " +
-            //    "WHERE mmsi = '" + mmsi + "'";
-            //dohOil.SqlCmd = sqlCmd;
-            //DataTable dt = dohOil.GetDataTable();
-            //if (dt != null && dt.Rows != null && dt.Rows.Count > 0)
-            //{
-            //    if (int.TryParse(dt.Rows[0]["oil_type"].ToString(), out oilType) == false)
-            //    {
-            //        oilType = 0;
-            //    }
-            //}
+            dohOil.Reset();
+            string sqlCmd =
+                "SELECT TOP 1 * " +
+                "FROM ship_reginfo " +
+                "WHERE mmsi = '" + mmsi + "'";
+            dohOil.SqlCmd = sqlCmd;
+            DataTable dt = dohOil.GetDataTable();
+            if (dt != null && dt.Rows != null && dt.Rows.Count > 0)
+            {
+                if (int.TryParse(dt.Rows[0]["oil_type"].ToString(), out oilType) == false)
+                {
+                    oilType = 0;
+                }
+            }
 
-            //{
-            //    dohOil.Reset();
-            //    sqlCmd =
-            //        "SELECT TOP 1 * " +
-            //        "FROM hsj_oil_density " +
-            //        "WHERE CompanyID = " + this.ComId + " " +
-            //        "AND OilTypeID = " + oilType.ToString();
-            //    dohOil.SqlCmd = sqlCmd;
-            //    dt = dohOil.GetDataTable();
-            //    if (dt != null && dt.Rows != null && dt.Rows.Count > 0)
-            //    {
-            //        if (double.TryParse(dt.Rows[0]["OilDensitySummer"].ToString(), out oilDensitySummer) == false)
-            //        {
-            //            oilDensitySummer = 0.84;
-            //        }
+            {
+                dohOil.Reset();
+                sqlCmd =
+                    "SELECT TOP 1 * " +
+                    "FROM hsj_oil_density " +
+                    "WHERE OilTypeID = " + oilType.ToString();
+                dohOil.SqlCmd = sqlCmd;
+                dt = dohOil.GetDataTable();
+                if (dt != null && dt.Rows != null && dt.Rows.Count > 0)
+                {
+                    if (double.TryParse(dt.Rows[0]["OilDensitySummer"].ToString(), out oilDensitySummer) == false)
+                    {
+                        oilDensitySummer = OIL_DENSITY_SUMMER;
+                    }
 
-            //        if (double.TryParse(dt.Rows[0]["OilDensityWinter"].ToString(), out oilDensityWinter) == false)
-            //        {
-            //            oilDensityWinter = 0.86;
-            //        }
-            //    }
-            //}
+                    if (double.TryParse(dt.Rows[0]["OilDensityWinter"].ToString(), out oilDensityWinter) == false)
+                    {
+                        oilDensityWinter = OIL_DENSITY_WINTER;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// getRealtimeStat
+        /// </summary>
+        private void getRealtimeStat()
+        {
+            List<RealtimeInfo> items = new List<RealtimeInfo>();
+            RealtimeInfo ri = null;
+            //DateTime datetime=DateTime.Now;
+            //string endTime = datetime.AddMinutes(Const.LIMIT).ToString("yyyy-MM-dd HH:mm:ss");
+            //string startTime = datetime.AddMinutes(-Const.MINUTES).ToString("yyyy-MM-dd HH:mm:ss");
+
+            //调用油耗数据操作类
+            dohOil.Reset();
+            dohOil.SqlCmd =
+
+                "SELECT * FROM ( " +
+                "SELECT " +
+                "MAX(stime) as stime, " +
+                "AVG(speed) as speed, " +
+                "MAX(mmsi) as mmsi," +
+                "AVG(pos_x) as pos_x, " +
+                "AVG(pos_y) as pos_y, " +
+                "AVG(llun_rps) as llun_rps, " +
+                "AVG(rlun_rps) as rlun_rps, " +
+                "AVG(lmain_oil_gps) as lmain_oil_gps, " +
+                "AVG(lasist_oil_gps) as lasist_oil_gps, " +
+                "AVG(rmain_oil_gps) as rmain_oil_gps, " +
+                "AVG(rasist_oil_gps) as rasist_oil_gps, " +
+                "DATEDIFF(MINUTE, MAX(stime), GETDATE()) as intervalMinute, " +
+                "AVG(drift_interval) as drift_interval " +
+                "FROM " +
+                "( " +
+                "SELECT TOP 3 * " +
+                "FROM ship_dynamicinfo " +
+                "WHERE mmsi='" + mmsi + "' " +
+                "ORDER BY stime DESC " +
+                ")  AS dynamic_top3 " +
+                ") AS result_left " +
+                "INNER JOIN " +
+                "( " +
+                "SELECT " +
+                "TOP 1 " +
+                "stime as t1stime, " +
+                "lmpg_status, " +
+                "rmpg_status " +
+                "FROM ship_dynamicinfo " +
+                "WHERE mmsi='" + mmsi + "' " +
+                "ORDER BY stime DESC " +
+                ") AS dynamic_top1 " +
+                "ON result_left.stime = dynamic_top1.t1stime";
+
+            DataTable dt = dohOil.GetDataTable();
+            //绑定对象
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                ri = new RealtimeInfo();
+                ri.mmsi = dt.Rows[0]["mmsi"].ToString();
+                ri.stime = dt.Rows[0]["stime"].ToString();
+                ri.pos_x = dt.Rows[0]["pos_x"].ToString();
+                ri.pos_y = dt.Rows[0]["pos_y"].ToString();
+                ri.speed = dt.Rows[0]["speed"].ToString();
+                ri.llunrps = dt.Rows[0]["llun_rps"].ToString();
+                ri.rlunrps = dt.Rows[0]["rlun_rps"].ToString();
+                try
+                {
+                    if (string.IsNullOrEmpty(ri.stime) == false)
+                    {
+                        DateTime stime = Convert.ToDateTime(ri.stime);
+                        double oilDensity = getOilDensity(stime);
+
+                        if (dt.Rows[0]["lmain_oil_gps"] != DBNull.Value)
+                            ri.lmain_gps = (Convert.ToDouble(dt.Rows[0]["lmain_oil_gps"].ToString()) * oilDensity).ToString();
+                        if (dt.Rows[0]["rmain_oil_gps"] != DBNull.Value)
+                            ri.rmain_gps = (Convert.ToDouble(dt.Rows[0]["rmain_oil_gps"].ToString()) * oilDensity).ToString();
+                        if (dt.Rows[0]["lasist_oil_gps"] != DBNull.Value)
+                            ri.lasist_gps = (Convert.ToDouble(dt.Rows[0]["lasist_oil_gps"].ToString()) * oilDensity).ToString();
+                        if (dt.Rows[0]["rasist_oil_gps"] != DBNull.Value)
+                            ri.rasist_gps = (Convert.ToDouble(dt.Rows[0]["rasist_oil_gps"].ToString()) * oilDensity).ToString();
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+
+                ri.interval_minute = dt.Rows[0]["intervalMinute"].ToString();
+                ri.drift_interval = dt.Rows[0]["drift_interval"].ToString();
+                ri.lmpg_status = dt.Rows[0]["lmpg_status"].ToString();
+                ri.rmpg_status = dt.Rows[0]["rmpg_status"].ToString();
+                ri.lrun_time = getLatestDuration(DURATION_TYPE.RUNNING_TIME_LEFT).ToString();
+                ri.rrun_time = getLatestDuration(DURATION_TYPE.RUNNING_TIME_RIGHT).ToString();
+                ri.sail_time = getLatestDuration(DURATION_TYPE.SAIL_TIME).ToString();
+                items.Add(ri);
+            }
+
+            dtHelp.ResutJsonStr((Object)items);
+        }
+
+        /// <summary>
+        /// getOilDensity
+        /// </summary>
+        /// <param name="dtBTime">season time</param>
+        /// <returns></returns>
+        private double getOilDensity(DateTime dtBTime)
+        {
+            if (SEASON_SUMMER <= dtBTime.Month && dtBTime.Month < SEASON_WINTER)
+            {
+                return oilDensitySummer;
+            }
+            else
+            {
+                return oilDensityWinter;
+            }
+        }
+
+        /// <summary>
+        /// getLatestDuration
+        /// </summary>
+        /// <param name="type">Duration Type</param>
+        /// <returns></returns>
+        private double getLatestDuration(DURATION_TYPE type)
+        {
+            TimeSpan sailingTimeSpan = TimeSpan.Zero;
+            dohOil.Reset();
+
+            string durationTable = getDurationTable(type);
+            string sqlCmd =
+                "SELECT TOP 1 * " +
+                "FROM " + durationTable + " " +
+                "ORDER BY startTime DESC";
+            dohOil.SqlCmd = sqlCmd;
+            DataTable dt = dohOil.GetDataTable();
+
+            if (dt != null && dt.Rows != null && dt.Rows.Count > 0)
+            {
+                if (dt.Rows[0]["endTime"] == DBNull.Value)
+                {
+                    DateTime recordBTime = Convert.ToDateTime(dt.Rows[0]["startTime"].ToString());
+                    DateTime recordETime = DateTime.Now;
+
+                    sailingTimeSpan = recordETime - recordBTime;
+                }
+            }
+
+            return sailingTimeSpan.TotalHours;
+        }
+
+        private string getDurationTable(DURATION_TYPE type)
+        {
+            string durationTable = "dbo.Ship_Voyeges3";
+            switch (type)
+            {
+                case DURATION_TYPE.RUNNING_TIME_LEFT:
+                    durationTable = "dbo.ship_voyeges_llun";
+                    break;
+
+                case DURATION_TYPE.RUNNING_TIME_RIGHT:
+                    durationTable = "dbo.ship_voyeges_rlun";
+                    break;
+
+                case DURATION_TYPE.RUNNING_TIME:
+                    durationTable = "dbo.Ship_Voyeges2";
+                    break;
+
+                case DURATION_TYPE.SAIL_TIME:
+                default:
+                    break;
+            }
+
+            return durationTable;
         }
     }
 }
